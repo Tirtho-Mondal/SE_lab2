0476fb15ba49a0d1fdb83f338fa4fd9c4a583f89
Tirtho <tirthomondal.2001@gmail.com>
2024-01-25 15:27:36 +0600

    singleton converted

---
+++ SingletonShape.java
@@ -0,0 +1,25 @@
+public class SingletonShape {
+    private static final SingletonShape instance = new SingletonShape();
+
+    private SingletonShape() {
+    }
+
+    public static SingletonShape getSingle() {
+        return instance;
+    }
+
+    public Shape getShape(String shapeType) {
+        if (shapeType == null) {
+            return null;
+        }
+        if (shapeType.equalsIgnoreCase("CIRCLE")) {
+            return new Circle();
+        }
+        if (shapeType.equalsIgnoreCase("RECTANGLE")) {
+            return new Rectangle();
+        } else if (shapeType.equalsIgnoreCase("SQUARE")) {
+            return new Square();
+        }
+        return null;
+    }
+}
